641fb931090fce8de8dc00b54941d775427464d6
Codefaa <facundogarcia1999@outlook.com>
2024-03-06 18:52:37 -0300

    cv actualizado

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
             <nav className='contacto-enlaces'>
                 <a className='link' href="https://www.linkedin.com/in/facundo-garcia-312b73297/" target='_blank' rel="noopener noreferrer" data-texto="LinkedIn"><i className="bi bi-linkedin"></i></a>
                 <a className='link' href="https://github.com/Codefaa" target='_blank' rel="noopener noreferrer" data-texto="GitHub"><i className="bi bi-github"></i></a>
-                <a className='link' href="https://drive.google.com/file/d/1pDeu9BCswRvl5Ga_oRCTbV9ophszKZTi/view?usp=drive_link" target='_blank' rel="noopener noreferrer" data-texto="CV"><i className="bi bi-file-person-fill"></i></a>
+                <a className='link' href="https://drive.google.com/file/d/1pvy-ho58gIYw24o9P1e9qKEibd80QfYO/view?usp=sharing" target='_blank' rel="noopener noreferrer" data-texto="CV"><i className="bi bi-file-person-fill"></i></a>
             </nav>
             <p>Facundo Garcia</p>
         </footer>

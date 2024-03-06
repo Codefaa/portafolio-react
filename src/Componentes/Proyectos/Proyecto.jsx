5d4c6e3a2886518e3b6e92823415e4ebdbee2713
Codefaa <facundogarcia1999@outlook.com>
2024-03-06 03:04:36 -0300

    modificaciones

--- src/Componentes/Proyectos/Proyecto.jsx
+++ src/Componentes/Proyectos/Proyecto.jsx
@@ -1,9 +1,9 @@
 import './Proyecto.css'
 import coffee from '../Imagenes/coffee.png'
-import quote from '../Imagenes/quote.png'
+import commerce  from '../Imagenes/e-commerce.png'
+import commerceMern from '../Imagenes/e-commerce-mern.png'
+import peliculas from '../Imagenes/peliculas-app.png'
 import markdown from '../Imagenes/markdown.png'
-import drummachine from '../Imagenes/drummachine.png'
-import calculadora from '../Imagenes/calculadora.png'
 import clock from '../Imagenes/clock.png'
 import { useTranslation } from 'react-i18next'
 
@@ -23,49 +23,56 @@ function Proyecto( {id} ) {
                         <h3 className='carta-titulo'>{t("proyecto.coffee")}</h3>
                         <h5 className='carta-parrafo'>{t("proyecto.descripcioncoffe")}</h5>
                         <div className='carta-tecnologias'>
-                            <button className="carta-boton">React</button>
+                            <button className="carta-boton">HTML</button>
+                            <button className="carta-boton">CSS</button>
                         </div>
                     </div>
                 </div>
                 <div className="carta">
-                    <img className='carta-imagen' src={quote} alt="quote" />
+                    <img className='carta-imagen' src={commerce} alt="ecommerce" />
                     <div className='carta-contenido'>
-                        <a className='carta-enlace' href="https://codepen.io/Codefaa/pen/ExMXdxg" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
-                        <h3 className='carta-titulo'>{t("proyecto.quote")}</h3>
-                        <h5 className='carta-parrafo'>{t("proyecto.descripcionquote")}</h5>
+                        <a className='carta-enlace' href="https://codefaa.github.io/e-commerce-apifakestore/" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
+                        <h3 className='carta-titulo'>{t("proyecto.ecommerce")}</h3>
+                        <h5 className='carta-parrafo'>{t("proyecto.descripcionecommerce")}</h5>
                         <div className='carta-tecnologias'>
                             <button className="carta-boton">React</button>
                         </div>
                     </div>
                 </div>
                 <div className="carta">
-                    <img className='carta-imagen' src={markdown} alt="markdown" />
+                    <img className='carta-imagen' src={commerceMern} alt="ecommerceMern" />
+                    <div className="carta-contenido">
+                        <a className='carta-enlace' href="https://github.com/Codefaa/e-commerce-mern" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
+                        <h3 className='carta-titulo'>{t("proyecto.ecommerceMern")}</h3>
+                        <h5 className='carta-parrafo'>{t("proyecto.descripcionecommerceMern")}</h5>
+                        <div className='carta-tecnologias'>
+                            <button className="carta-boton">MongoDB</button>
+                            <button className="carta-boton">Express</button>
+                            <button className="carta-boton">React</button>
+                            <button className="carta-boton">NodeJS</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="carta">
+                    <img className='carta-imagen' src={peliculas} alt="peliculas" />
+                    <div className="carta-contenido">
+                        <a className='carta-enlace' href="https://github.com/Codefaa/peliculas-mern" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
+                        <h3 className='carta-titulo'>{t("proyecto.peliculas")}</h3>
+                        <h5 className='carta-parrafo'>{t("proyecto.descripcionpeliculas")}</h5>
+                        <div className='carta-tecnologias'>
+                        <button className="carta-boton">MongoDB</button>
+                            <button className="carta-boton">Express</button>
+                            <button className="carta-boton">React</button>
+                            <button className="carta-boton">NodeJS</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="carta">
+                   <img className='carta-imagen' src={markdown} alt="markdown" />
                     <div className="carta-contenido">
                         <a className='carta-enlace' href="https://codepen.io/Codefaa/pen/gOExOXa" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
                         <h3 className='carta-titulo'>{t("proyecto.markdown")}</h3>
                         <h5 className='carta-parrafo'>{t("proyecto.descripcionmarkdown")}</h5>
-                        <div className='carta-tecnologias'>
-                            <button className="carta-boton">React</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="carta">
-                    <img className='carta-imagen' src={drummachine} alt="drummachine" />
-                    <div className="carta-contenido">
-                        <a className='carta-enlace' href="https://codepen.io/Codefaa/pen/oNVebOZ" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
-                        <h3 className='carta-titulo'>{t("proyecto.drummachine")}</h3>
-                        <h5 className='carta-parrafo'>{t("proyecto.descripciondrummachine")}</h5>
-                        <div className='carta-tecnologias'>
-                            <button className="carta-boton">React</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="carta">
-                   <img className='carta-imagen' src={calculadora} alt="calculadora" />
-                    <div className="carta-contenido">
-                        <a className='carta-enlace' href="https://codepen.io/Codefaa/pen/xxBLVYL" target='_blank' rel="noopener noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
-                        <h3 className='carta-titulo'>{t("proyecto.calculator")}</h3>
-                        <h5 className='carta-parrafo'>{t("proyecto.descripcioncalculator")}</h5>
                         <div className='carta-tecnologias'>
                             <button className="carta-boton">React</button>
                         </div>
